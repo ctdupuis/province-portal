568a7c2418634928b82e9a10ce429799569dc9e7
ctdupuis <ctdupuis7@gmail.com>
2020-10-03 14:07:00 -0500

    placeholders/begin styling for dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,10 +5,33 @@ export default class Dashboard extends Component {
         return (
             <section className="dash-container">
                 <header className="dash-header">
-                    <div>Announcements</div>
+                    <h3>Posts</h3>
                 </header>
-                <article className="announcement-content">
-                    
+                <article className="post-container">
+                    <div className="post-content">
+                        <div className="post-author">
+                            CTD
+                        </div>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div className="post-content">
+                        <div className="post-author">
+                            ESS
+                        </div>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div className="post-content">
+                        <div className="post-author">
+                            MP
+                        </div>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
                 </article>
                 {/* <h3>You are signed in as {this.props.currentUser.username}</h3> */}
                 {/* Drop the AnnouncementsContainer here */}
