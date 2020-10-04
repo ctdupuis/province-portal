02c62342377d872f3e9a156ab94915dea8ea2ab9
ctdupuis <ctdupuis7@gmail.com>
2020-10-03 21:53:07 -0500

    working on styling comments section

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,6 +15,32 @@ export default class Dashboard extends Component {
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
+                        <div className="comment-container">
+                            <ul className="comment-list">
+                                <li className="comment-head">Comments</li>
+                                <div className="comment">
+                                    <li>Comment</li>
+                                </div>
+                                <div className="comment">
+                                    <li>Comment</li>
+                                </div>
+                                <div className="comment">
+                                    <li>Comment</li>
+                                </div>
+                                <div className="comment">
+                                    <li>Comment</li>
+                                </div>
+                                <div className="comment">
+                                    <div className="input-container">
+                                        <textarea type="text" className="comment-input" />
+                                    </div>
+                                    <div className="btn-container">
+                                        <button className="comment-submit">Post Comment</button>    
+                                    </div>    
+                                </div>
+                            </ul>
+
+                        </div>
                     </div>
                     <div className="post-content">
                         <div className="post-author">
