9a92891cb3d1573d02694dba7760b828b3451550
ctdupuis <ctdupuis7@gmail.com>
2020-12-30 22:39:26 -0600

    adjust destructuring of Message, add more styling options

--- src/components/patient-services/Message.js
+++ src/components/patient-services/Message.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export default function Message({ message }) {
+    const {id, text, author, created, updated} = message
     return (
         <div className="message">
-            {message.text}
+            <div className="message-body">
+                {text}
+            </div>
+            <div className="message-metadata">
+                <div className="message-author">
+                    {author}
+                </div>
+                <div className="message-timestamps">
+                    {created}
+                </div>
+            </div>
         </div>
     )
 }
