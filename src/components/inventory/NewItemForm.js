a4f6beeae4fd6687bb3c3a285d78cac9315f59ea
ctdupuis <ctdupuis7@gmail.com>
2021-01-16 23:02:10 -0600

    Added category for items to NewItemForm

--- src/components/inventory/NewItemForm.js
+++ src/components/inventory/NewItemForm.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class NewItemForm extends Component {
     state = {
-        items: [{product_name: "", qty: 0, units: ""}]
+        items: [{product_name: "", qty: 0, units: "", category: ""}]
     }
 
     handleChange = event => {
-        if (["product_name", "qty", "units"].includes(event.target.className)) {
+        if (["product_name", "qty", "units", "category"].includes(event.target.className)) {
             let items = [...this.state.items]
             items[event.target.dataset.id][event.target.className] = event.target.value
             this.setState({ items })
@@ -22,15 +22,25 @@ export default class NewItemForm extends Component {
         }));
     }
 
+    setCity = (event) => {
+        this.setState({
+          location: {
+            ...this.state.location,
+            city: event.target.value,
+          },
+        });
+      };
+
     render() {
         let items = this.state.items
         return (
             <div className="user-info-content">
                 {items.map((val, idx) => {
-                    let itemID = `item-${idx}`, qtyID = `qty-${idx}`, unitsID = `units-${idx}`
+                    let itemID = `item-${idx}`, qtyID = `qty-${idx}`, unitsID = `units-${idx}`, categoryID = `category-${idx}`
                     return(
                         <fieldset id="new-item" key={idx}>
                             <legend>Item #{idx + 1}</legend>
+                            <div className="fieldset-flex">
                             <label htmlFor={itemID}>Product Name</label>
                             <input
                                 type="text"
@@ -62,10 +72,25 @@ export default class NewItemForm extends Component {
                                 value={items[idx].units}
                                 onChange={this.handleChange}
                             />
+                            <select
+                                id={categoryID}
+                                data-id={idx}
+                                name={categoryID}
+                                className="category"
+                                value={items[idx].category}
+                                onChange={this.handleChange}
+                            >
+                                <option>Select a category...</option>
+                                <option value="Shipping Supplies">Shipping Supplies</option>
+                                <option value="Cleaning Supplies">Cleaning Supplies</option>
+                                <option value="Office Supplies">Office Supplies</option>
+                            </select>
+                            </div>
                         </fieldset>
                     )
                 })}
                 <button className="green-btn" onClick={this.addItem}>Add Another Item</button>
+                <button className="green-btn">Submit Item(s)</button>
             </div>
         )
     }
