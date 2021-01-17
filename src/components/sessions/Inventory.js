20f615d0bc88e67dd1751126fb516c3084001b76
ctdupuis <ctdupuis7@gmail.com>
2021-01-16 22:18:51 -0600

    setting up NewItemForm

--- src/components/sessions/Inventory.js
+++ src/components/sessions/Inventory.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Inventory extends Component {
     componentDidMount() {
         this.props.getItems();
+    }
+
+    state = {
+        localItems: [{product_name: "", qty: 0, units: ""}]
     }
 
     tableHeaders = () => {
@@ -30,13 +34,47 @@ export default class Inventory extends Component {
         )
     }
 
+    handleChange = event => {
+        if (["product_name", "qty", "units"].includes(event.target.className)) {
+            let localItems = [...this.state.localItems]
+            localItems[event.target.dataset.id][event.target.className] = event.target.value
+            this.setState({ localItems })
+        } else {
+            this.setState({ [event.target.name]: event.target.value })
+        }
+    }
+
     render() {
+        let localItems = this.state.localItems
         const items = this.props.items
         const shippingItems = items.filter(item => item.category === "Shipping Supplies")
         const cleaningItems = items.filter(item => item.category === "Cleaning Supplies")
         return (
             <section className="dash-container">
                 <div className="dash-content">
+                    {/* <div className="user-info-title">
+                        <h3>Add A New Item</h3>
+                    </div> */}
+                    <div className="user-info-content">
+                        {localItems.map((val, idx) => {
+                            let itemID = `item-${idx}`, qtyID = `qty-${idx}`, unitsID = `units-${idx}`
+                            return(
+                                <fieldset key={idx}>
+                                    <legend>Item #{idx + 1}</legend>
+                                    <label htmlFor={itemID}>Product Name</label>
+                                    <input
+                                        type="text"
+                                        name={itemID}
+                                        className="product_name"
+                                        id={itemID}
+                                        data-id={idx}
+                                        value={localItems[idx].product_name}
+                                        onChange={this.handleChange}
+                                    />
+                                </fieldset>
+                            )
+                        })}
+                    </div>
                     <div class="user-info-title">
                         <h3>Shipping Supplies</h3>
                     </div>
