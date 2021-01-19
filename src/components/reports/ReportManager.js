a04c46186989a1a84465040a20bcd574b2b8b6be
ctdupuis <ctdupuis7@gmail.com>
2021-01-18 19:55:32 -0600

    started a boilerplate for ReportManager

--- src/components/reports/ReportManager.js
+++ src/components/reports/ReportManager.js
@@ -4,9 +4,26 @@ export default class ReportManager extends Component {
     render() {
         return (
             <section className="dash-container">
-                <header className="dash-header">
-                    <h3>Report Manager | Under Construction</h3>
-                </header>
+                <div className="dash-content">
+                    <div className="user-info-title">
+                        <h3>Select the type of report you need</h3>
+                    </div>
+
+                    <div className="user-info-content">
+                        <select id="reports">
+                            <option>Select a report...</option>
+                            <option value="Mileage">Mileage Report</option>
+                            <option value="Check">Check Report</option>
+                            <option value="FedEx">FedEx Issues Report</option>
+                        </select>
+                        <br />
+                        <label htmlFor="start-date">Start Date</label>
+                        <input type="date" id="start-date" />
+                        <label htmlFor="end-date">End Date</label>
+                        <input type="date" id="end-date" />
+                    </div>
+                    <button className="green-btn">Generate Report</button>
+                </div>
             </section>
         )
     }
