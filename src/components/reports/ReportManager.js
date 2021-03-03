b2d0ed5b162113a048ab87971c6e94cbd1cc18db
ctdupuis <ctdupuis7@gmail.com>
2021-03-02 21:02:24 -0600

    setting up state in ReportManager

--- src/components/reports/ReportManager.js
+++ src/components/reports/ReportManager.js
@@ -3,6 +3,23 @@ import '../../stylesheets/reports.css'
 import CheckLog from './CheckLog';
 
 export default class ReportManager extends Component {
+    state = {
+        start_date: "",
+        end_date: "",
+        type: ""
+    }
+
+    onChange = event => {
+        this.setState({ [event.target.name]: [event.target.value] })
+    }
+
+    setType = event => {
+        this.setState({
+            ...this.state,
+            type: event.target.value
+        })
+    }
+
     render() {
         return (
             <section className="dash-container">
@@ -15,17 +32,31 @@ export default class ReportManager extends Component {
                     </div>
 
                     <div className="user-info-content">
+
                         <select id="reports">
                             <option>Select a report...</option>
                             <option value="Mileage">Mileage Report</option>
                             <option value="Check">Check Report</option>
-                            <option value="FedEx">FedEx Issues Report</option>
                         </select>
+
                         <br />
+
                         <label htmlFor="start-date">Start Date</label>
-                        <input type="date" id="start-date" />
+                        <input 
+                            type="date" 
+                            name="start_date" 
+                            id="start-date" 
+                            value={this.state.start_date}
+                        />
+
                         <label htmlFor="end-date">End Date</label>
-                        <input type="date" id="end-date" />
+                        <input 
+                            type="date" 
+                            name="end_date" 
+                            id="end-date" 
+                            value={this.state.end_date}
+                        />
+                        
                     </div>
                     <button className="green-btn">Generate Report</button>
                 </div>
