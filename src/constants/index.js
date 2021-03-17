d3a153bc679605214d3876636326426a452de315
ctdupuis <ctdupuis7@gmail.com>
2021-03-17 18:53:48 -0500

    back to development

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
-export const API_ROOT = "https:province-api.herokuapp.com"
-export const API_WS_ROOT = "ws://province-api.herokuapp.com/cable"
+// export const API_ROOT = "https:province-api.herokuapp.com"
+// export const API_WS_ROOT = "ws://province-api.herokuapp.com/cable"
+
+export const API_ROOT = "http://localhost:5000"
+export const API_WS_ROOT = "ws://localhost:5000/cable"
