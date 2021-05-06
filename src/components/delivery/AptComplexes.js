0100dc425ad418d277ecf50b08b00e351e1f1463
ctdupuis <ctdupuis7@gmail.com>
2021-05-05 21:18:51 -0500

    added addresses that require gate code

--- src/components/delivery/AptComplexes.js
+++ src/components/delivery/AptComplexes.js
@@ -6,8 +6,14 @@ export default function AptComplexes() {
             <fieldset className="info">
                 <legend>Apartment Complexes or Gated Communities</legend>
                 <ul className="li-items">
+                    <li>1521 Camellia Blvd</li>
+                    <li>1000-1200 Robley Dr</li>
+                    <li>115 Republic Ave</li>
+                    <li>115 Liberty Dr</li>
                     <li>201 Prescott Blvd</li>
-                    <li>215 Republic Ave</li>
+                    <li>210 Polly Ln</li>
+                    <li>2314 Kaliste Saloom Rd</li>
+                    <li>101 Shallowford Dr</li>
                 </ul>
             </fieldset>
         </div>
