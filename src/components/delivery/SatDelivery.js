f1c6316cdbbf1c962e823c21c2683be838facceb
ctdupuis <ctdupuis7@gmail.com>
2021-05-05 21:15:15 -0500

    added SatDelivery locations with styling

--- src/components/delivery/SatDelivery.js
+++ src/components/delivery/SatDelivery.js
@@ -6,8 +6,21 @@ export default function SatDelivery() {
             <fieldset className="info">
                 <legend>Sat Delivery Not Supported</legend>
                 <ul className="li-items">
-                    <li>Baldwin</li>
-                    <li>Boswick</li>
+                <li>Basile</li>
+                <li>Baldwin</li>
+                <li>Cut Off</li>
+                <li>Dulac</li>
+                <li>Fort Necessity</li>
+                <li>Glenmora</li>
+                <li>Kaplan</li>
+                <li>Lockport</li>
+                <li>Luling</li>
+                <li>Madisonville</li>
+                <li>Pierre Part</li>
+                <li>Pollock</li>
+                <li>Ponchatoula</li>
+                <li>Reserve</li>
+                <li>Saint Francisville</li>
                 </ul>
             </fieldset>
         </div>
