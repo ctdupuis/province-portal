f0dfee578bd9022c671646216c930bf7f152b1f3
ctdupuis <ctdupuis7@gmail.com>
2021-05-23 20:23:43 -0500

    more flex containers for forms

--- src/components/inventory/NewItemForm.js
+++ src/components/inventory/NewItemForm.js
@@ -42,90 +42,95 @@ export default class NewItemForm extends Component {
         let items = this.state.items
         return (
             <div className="user-info-content">
-                {items.map((val, idx) => {
-                    let itemID = `item-${idx}`, qtyID = `qty-${idx}`, unitsID = `units-${idx}`, categoryID = `category-${idx}`
-                    return(
-                        <fieldset id="new-item" key={idx}>
-                            <legend>Item #{idx + 1}</legend>
+                <div className="flex-container" style={{ flexWrap: "wrap"}}>
+                    {items.map((val, idx) => {
+                        let itemID = `item-${idx}`, qtyID = `qty-${idx}`, unitsID = `units-${idx}`, categoryID = `category-${idx}`
+                        return(
+                            <fieldset id="new-item" key={idx}>
+                                <legend>Item #{idx + 1}</legend>
 
-                            <div className="fieldset-flex">
+                                <div className="fieldset-flex">
 
-                                <div className="form-group">
-                                    <label htmlFor={itemID}>Product Name</label>
-                                    <input
-                                        type="text"
-                                        name={itemID}
-                                        className="product_name"
-                                        id={itemID}
-                                        data-id={idx}
-                                        value={items[idx].product_name}
-                                        onChange={this.handleChange}
-                                        required={true}
-                                    />
+                                    <div className="form-group">
+                                        <label htmlFor={itemID}>Product Name</label>
+                                        <input
+                                            type="text"
+                                            name={itemID}
+                                            className="product_name"
+                                            id={itemID}
+                                            data-id={idx}
+                                            value={items[idx].product_name}
+                                            onChange={this.handleChange}
+                                            required={true}
+                                            />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor={qtyID}>Quantity</label>
+                                        <input 
+                                            type="number"
+                                            name={qtyID}
+                                            min="0"
+                                            max="100"
+                                            step="0.1"
+                                            className="qty"
+                                            id={qtyID}
+                                            data-id={idx}
+                                            value={items[idx].qty}
+                                            onChange={this.handleChange}
+                                            required={true}
+                                            />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor={unitsID}>Unit of Measurement</label>
+                                        <input
+                                            type="text"
+                                            name={unitsID}
+                                            className="units"
+                                            id={unitsID}
+                                            data-id={idx}
+                                            value={items[idx].units}
+                                            onChange={this.handleChange}
+                                            required={true}
+                                            />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor={categoryID}>Item Type</label>
+                                        <select
+                                            id={categoryID}
+                                            data-id={idx}
+                                            name={categoryID}
+                                            className="category"
+                                            value={items[idx].category}
+                                            onChange={this.handleChange}
+                                            required={true}
+                                            >
+                                            <option>Select a category...</option>
+                                            <option value="Shipping Supplies">Shipping Supplies</option>
+                                            <option value="Cleaning Supplies">Cleaning Supplies</option>
+                                            <option value="Office Supplies">Office Supplies</option>
+                                        </select>
+                                    </div>
+
                                 </div>
+                            </fieldset>
+                        )
+                    })}
+                </div>
 
-                                <div className="form-group">
-                                    <label htmlFor={qtyID}>Quantity</label>
-                                    <input 
-                                        type="number"
-                                        name={qtyID}
-                                        min="0"
-                                        max="100"
-                                        step="0.1"
-                                        className="qty"
-                                        id={qtyID}
-                                        data-id={idx}
-                                        value={items[idx].qty}
-                                        onChange={this.handleChange}
-                                        required={true}
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor={unitsID}>Unit of Measurement</label>
-                                    <input
-                                        type="text"
-                                        name={unitsID}
-                                        className="units"
-                                        id={unitsID}
-                                        data-id={idx}
-                                        value={items[idx].units}
-                                        onChange={this.handleChange}
-                                        required={true}
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor={categoryID}>Item Type</label>
-                                    <select
-                                        id={categoryID}
-                                        data-id={idx}
-                                        name={categoryID}
-                                        className="category"
-                                        value={items[idx].category}
-                                        onChange={this.handleChange}
-                                        required={true}
-                                        >
-                                        <option>Select a category...</option>
-                                        <option value="Shipping Supplies">Shipping Supplies</option>
-                                        <option value="Cleaning Supplies">Cleaning Supplies</option>
-                                        <option value="Office Supplies">Office Supplies</option>
-                                    </select>
-                                </div>
-
-                            </div>
-                        </fieldset>
-                    )
-                })}
-                <button className="green-btn" onClick={this.addItem}>
-                    Add Another Item
-                </button>
-                <button className="green-btn" onClick={this.popItem}>
-                    Remove Item
-                </button>
-                <button className="green-btn" onClick={() => this.handleSubmit(this.state)}>
-                    Submit Item(s)
-                </button>
+                <div className="flex-container">
+                    <button className="green-btn" onClick={this.addItem}>
+                        Add Another Item
+                    </button>
+                    <button className="green-btn" onClick={this.popItem}>
+                        Remove Item
+                    </button>
+                    <button className="green-btn" onClick={() => this.handleSubmit(this.state)}>
+                        Submit Item(s)
+                    </button>
+                </div>
             </div>
         )
     }
